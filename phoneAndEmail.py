0611e6173b62b37549e79a077df7834d8a838cb2
Brian <brian.clauser@mhc.org>
2019-11-27 23:38:26 -0500

    type

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -20,7 +20,7 @@ emailRegex = re.compile(r'''(
     @                           # @ symbol
     [a-zA-Z0-9.-]+              # domain name
     (\.[a-zA-Z]{2,4})           # dot-something
-))''', re.VERBOSE)
+)''', re.VERBOSE)
 
 # Find matches in clipboard text.
 text = str(pyperclip.paste())
